0f4539d11c64440dbbac73bdea1542387b1aa94f
David Crocker <david@crocker.im>
2020-12-12 11:24:15 -0500

    Day 12 refactor

--- 2020/12.hs
+++ 2020/12.hs
@@ -1,65 +1,51 @@
-data Action = N | S | E | W | L | R | F deriving (Eq, Read, Show)
-data Direction = North | East | South | West deriving (Bounded, Enum, Eq, Show)
-type Step = (Action,Int)
-type Instructions = [Step]
-type Delta = (Int,Int)
-type Position = (Int,Int)
-type ShipState = (Direction,Position)
-type State = (Position,ShipState)
+import Control.Arrow ( first, second )
 
-parse :: String -> Instructions
-parse = map readLine . lines
-    where readLine (act:num) = (read [act], read num)
+data Mode = Position | Heading
+data Instruction = Translate Vector | Rotate Int | Forward Int
+type Vector = (Int,Int)
+-- A ship has a heading ("waypoint") and a position
+type Ship = (Vector,Vector)
 
-turnLeft :: Direction -> Direction
-turnLeft dir = if dir == minBound then maxBound else pred dir
+parse :: String -> [Instruction]
+parse = map readInstruction . lines
 
-turnRight :: Direction -> Direction
-turnRight dir = if dir == maxBound then minBound else succ dir
+readInstruction :: String -> Instruction
+readInstruction (action:number) =
+    let i = read number in
+    case action of
+        'N' -> Translate ( 0,  i)
+        'S' -> Translate ( 0, -i)
+        'E' -> Translate ( i,  0)
+        'W' -> Translate (-i,  0)
+        'L' -> Rotate    (-(div i 90))
+        'R' -> Rotate      (div i 90)
+        'F' -> Forward   i
 
-move :: Delta -> Position -> Position
-move (dx,dy) (x,y) = (x+dx,y+dy)
+addVec :: Vector -> Vector -> Vector
+addVec (x1,y1) (x2,y2) = (x1+x2, y1+y2)
 
-moveShip :: (Position -> Position) -> ShipState -> ShipState
-moveShip delta (dir,pos) = (dir, delta pos)
+-- Rotate a vector in 90-degree steps right (+) or left (-)
+rotate :: Int -> Vector -> Vector
+rotate r vec = iterate rot vec !! abs r
+    where direction = signum r
+          rot (x,y) = (direction * y, direction * (-x))
 
-moveDir :: Direction -> Int -> Position -> Position
-moveDir North i = move ( 0,  i)
-moveDir South i = move ( 0, -i)
-moveDir East  i = move ( i,  0)
-moveDir West  i = move (-i,  0)
+step :: Mode -> Instruction -> Ship -> Ship
+step Position (Translate t) = second (addVec t)
+step Heading  (Translate t) = first  (addVec t)
+step _        (Rotate r)    = first  (rotate r)
+step _        (Forward f)   = forward f
+    where forward f ((x,y),pos) = ((x, y), addVec (f*x, f*y) pos)
 
-stepShip :: Step -> ShipState -> ShipState
-stepShip (N,i) ship      = moveShip (moveDir North i) ship
-stepShip (S,i) ship      = moveShip (moveDir South i) ship
-stepShip (E,i) ship      = moveShip (moveDir East  i) ship
-stepShip (W,i) ship      = moveShip (moveDir West  i) ship
-stepShip (F,i) (dir,pos) = moveShip (moveDir dir   i) (dir,pos)
-stepShip (L,i) (dir,pos) = (iterate turnLeft  dir !! div i 90, pos)
-stepShip (R,i) (dir,pos) = (iterate turnRight dir !! div i 90, pos)
+-- Calculate the manhattan distance the ship will be from its starting
+-- location after following the instructions. The mode indicates whether
+-- the position or the heading of the ship is changed in response to
+-- north/south/east/west instructions.
+solve :: Mode -> Ship -> [Instruction] -> Int
+solve mode ship = manhattanDistance . snd . foldl (flip (step mode)) ship
+    where manhattanDistance (x,y) = abs x + abs y
 
-manhattanDistance :: Position -> Int
-manhattanDistance (x,y) = abs x + abs y
-
-rotateLeft :: Position -> Position
-rotateLeft (x,y) = (-y,x)
-
-rotateRight :: Position -> Position
-rotateRight (x,y) = (y,-x)
-
-step :: Step -> State -> State
-step (N,i) (waypoint,ship) = (moveDir North i waypoint, ship)
-step (S,i) (waypoint,ship) = (moveDir South i waypoint, ship)
-step (E,i) (waypoint,ship) = (moveDir East  i waypoint, ship)
-step (W,i) (waypoint,ship) = (moveDir West  i waypoint, ship)
-step (F,i) (waypoint,ship) = (waypoint, iterate (moveShip (move waypoint)) ship !! i)
-step (L,i) (waypoint,ship) = (iterate rotateLeft  waypoint !! div i 90, ship)
-step (R,i) (waypoint,ship) = (iterate rotateRight waypoint !! div i 90, ship)
-
-initialShipState = (East,(0,0))
-initialState = ((10,1),initialShipState)
-
-partOne = manhattanDistance . snd . foldl (flip stepShip) initialShipState
-partTwo = manhattanDistance . snd . snd . foldl (flip step) initialState
+partOne = solve Position ((1,0), (0,0))
+partTwo = solve Heading ((10,1), (0,0))
 
 main = interact $ (++ "\n") . show . sequence [partOne, partTwo] . parse
