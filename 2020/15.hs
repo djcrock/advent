8e1929664ebbb53c3e1f3d41ce8bd621728d642d
David Crocker <david@crocker.im>
2020-12-15 08:32:45 -0500

    Day 15 part 2
    Had to compile with optimizations to avoid getting OOM killed.

--- 2020/15.hs
+++ 2020/15.hs
@@ -1,4 +1,4 @@
-import Data.List ( elemIndex )
+import qualified Data.IntMap as Map
 
 parse :: String -> [Int]
 parse = map read . lines
@@ -6,12 +6,25 @@ parse = map read . lines
 -- Given a target position n and a stack of previous numbers,
 -- determine what the nth number spoken will be.
 countTo :: Int -> [Int] -> Int
-countTo n (x:xs) = if length (x:xs) == n then x else countTo n (next:x:xs)
-    where next = case elemIndex x xs of
-                   Nothing -> 0
-                   Just y  -> y + 1
+countTo target prev = countToInternal
+                        target
+                        (Map.fromList (zip (init prev) [1..]))
+                        (last prev)
+                        (length prev)
 
-partOne = countTo 2020 . reverse
-partTwo = partOne
+-- Uses a Map to keep track of just the most recent occurrence of each number.
+-- Compile with optimizations to avoid getting OOM killed. Thanks, Haskell :P
+countToInternal :: Int -> Map.IntMap Int -> Int -> Int -> Int
+countToInternal target recent prevVal prevIndex =
+  if prevIndex == target
+  then prevVal
+  else countToInternal target newRecent nextVal (prevIndex + 1)
+    where newRecent = Map.insert prevVal prevIndex recent
+          nextVal   = case Map.lookup prevVal recent of
+                        Nothing -> 0
+                        Just y  -> prevIndex - y
+
+partOne = countTo 2020
+partTwo = countTo 30000000
 
 main = interact $ (++ "\n") . show . sequence [partOne, partTwo] . parse
