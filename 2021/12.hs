bb5c18b9d195753f463fcce5ed47572976a1ce47
David Crocker <david@crocker.im>
2021-12-12 13:27:14 -0500

    Clean up 2021 day 12 solution

--- 2021/12.hs
+++ 2021/12.hs
@@ -1,45 +1,33 @@
-import Data.Char ( isUpper )
+import Data.Char ( isLower )
 import qualified Data.Map as Map
 
-data Node = Start
-          | Large String
-          | Small String
-          | End
-          deriving (Eq, Ord, Show)
-type Graph = Map.Map Node [Node]
-type Path = [Node]
+type Graph = Map.Map String [String]
+type Path = [String]
 
 parse :: String -> Graph
-parse = toGraph . map (sort . parseEdge) . lines
+parse = toGraph . map parseEdge . lines
     where parseEdge str =
-            ( toNode $ takeWhile (/='-') str
-            , toNode $ tail $ dropWhile (/='-') str )
-          toNode str
-            | str == "start"     = Start
-            | str == "end"       = End
-            | isUpper (head str) = Large str
-            | otherwise          = Small str
-          sort (x, Start) = (Start, x)
-          sort (End, x)   = (x, End)
-          sort x          = x
+            ( takeWhile (/='-') str
+            , tail $ dropWhile (/='-') str )
           toGraph = foldr addEdge Map.empty
-          addEdge (Start, to) = graphInsert Start to
-          addEdge (from, End) = graphInsert from End
-          addEdge (from, to)  = (graphInsert to from) . (graphInsert from to)
+          addEdge ("start", x) = graphInsert "start" x
+          addEdge (x, "start") = graphInsert "start" x
+          addEdge ("end", x)   = graphInsert x "end"
+          addEdge (x, "end")   = graphInsert x "end"
+          addEdge (x, y)       = (graphInsert x y) . (graphInsert y x)
           graphInsert from to = Map.insertWith (++) from [to]
 
 explore :: Bool -> Graph -> Path -> [Path]
 explore canRevisit graph path@(current:prev)
-    | current == End = [path]
+    | current == "end" = [path]
     | isRevisit && not canRevisit = []
     | otherwise = concatMap ((explore canRevisitNext graph) . (:path)) next
     where next = graph Map.! current
-          isRevisit = isSmall current && elem current prev
+          isRevisit = isSmall && elem current prev
           canRevisitNext = canRevisit && not isRevisit
-          isSmall (Small _) = True
-          isSmall _         = False
+          isSmall = isLower (head current) && current /= "start"
 
-partOne = length . (\g -> explore False g [Start])
-partTwo = length . (\g -> explore True  g [Start])
+partOne = length . (\g -> explore False g ["start"])
+partTwo = length . (\g -> explore True  g ["start"])
 
 main = interact $ (++ "\n") . show . sequence [partOne, partTwo] . parse
