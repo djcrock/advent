a718a65777169d94b3c28b4b5f1f9719c2c5a1cc
David Crocker <david@crocker.im>
2021-12-13 16:18:16 -0500

    Clean up 2021 day 04 solution

--- 2021/04.hs
+++ 2021/04.hs
@@ -1,52 +1,38 @@
-import Data.List ( (\\), inits, intersect, transpose )
+import Data.List ( (\\), intersect, transpose )
 import Text.Parsec
 
 type Parser a = Parsec String () a
 type Moves = [Int]
-type Row = [Int]
-type Board = [Row]
+type Board = [[Int]]
 
-natP :: Parser Int
-natP = read <$> many1 digit
-
-movesP :: Parser Moves
-movesP = sepBy natP (char ',')
-
-rowP :: Parser Row
-rowP = (many $ char ' ') *> sepBy1 natP (many1 $ char ' ')
-
-boardP :: Parser Board
-boardP = sepEndBy1 rowP newline
-
-inputParser :: Parser (Moves, [Board])
-inputParser = do
-    moves <- movesP
-    newline
-    newline
-    boards <- sepEndBy1 boardP newline
-    pure (moves, boards)
+inputP :: Parser (Moves, [Board])
+inputP = (,) <$> (movesP <* newline <* newline) <*> boardsP
+    where movesP = sepBy natP (char ',')
+          boardsP = sepEndBy1 boardP newline
+          boardP = sepEndBy1 rowP newline
+          rowP = (many $ char ' ') *> sepBy1 natP (many1 $ char ' ')
+          natP = read <$> many1 digit
 
 must (Right x) = x
 must (Left y)  = error $ show y
 
-isRowWinner :: Moves -> Row -> Bool
-isRowWinner moves row = intersect row moves == row
-
 isBoardWinner :: Moves -> Board -> Bool
-isBoardWinner moves board = any (isRowWinner moves) (board ++ transpose board)
+isBoardWinner moves board = any isRowWinner (board ++ transpose board)
+    where isRowWinner row = intersect row moves == row
 
 getWinners :: Moves -> Moves -> [Board] -> [(Moves, Board)]
 getWinners _ [] _ = []
-getWinners prevMoves (move:nextMoves) boards = map (moves,) winners ++ nextWinners
-    where moves       = move:prevMoves
-          winners     = filter (isBoardWinner moves) boards
-          nextWinners = getWinners moves nextMoves (boards \\ winners)
+getWinners prevMoves (move:nextMoves) boards =
+    (map (moves,) winners) ++ nextWinners
+        where moves       = move:prevMoves
+              winners     = filter (isBoardWinner moves) boards
+              nextWinners = getWinners moves nextMoves (boards \\ winners)
 
 score :: (Moves, Board) -> Int
 score (moves, board) = (head moves) * (sum unmarked)
     where unmarked = (concat board) \\ moves
 
-partOne (moves, boards) = score $ head $ getWinners [] moves boards
-partTwo (moves, boards) = score $ last $ getWinners [] moves boards
+partOne = score . head . uncurry (getWinners [])
+partTwo = score . last . uncurry (getWinners [])
 
-main = interact $ (++ "\n") . show . sequence [partOne, partTwo] . must . parse inputParser ""
+main = interact $ (++ "\n") . show . sequence [partOne, partTwo] . must . parse inputP ""
