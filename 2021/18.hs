397968c15f8cdf3bd92a01b99c1846feda797924
David Crocker <david@crocker.im>
2021-12-18 19:15:49 -0500

    Clean up 2021 day 18 solution

--- 2021/18.hs
+++ 2021/18.hs
@@ -1,77 +1,49 @@
-import Data.Maybe ( fromMaybe )
-import Text.Parsec 
+import Control.Applicative ( (<|>) )
+import Data.Functor ( (<&>) )
+import Text.Parsec ( char, digit, many1, newline, parse, sepEndBy, try )
 
-type Parser a = Parsec String () a
 data Number = Regular Int | Pair Number Number deriving Eq
 
-inputP :: Parser [Number]
-inputP = sepEndBy numberP newline
-    where numberP  = try regularP <|> pairP
-          pairP    = Pair
-                        <$> (char '[' *> numberP <* char ',')
-                        <*> (numberP <* char ']')
-          regularP = Regular <$> natP
-          natP     = read <$> many1 digit
-
-must (Right x) = x
-must (Left y)  = error $ show y
-
--- Iterate a function until the result stops changing
-fix :: Eq a => (a -> a) -> a -> a
-fix f x = if x == f x then x else fix f (f x)
-
-reduce :: Number -> Number
-reduce = fix (split . fix explode)
-
-explode :: Number -> Number
-explode num = case explode' 0 num of
-    Nothing            -> num
-    Just (exploded, _) -> exploded
+parseInput :: String -> [Number]
+parseInput = must . parse (sepEndBy numP newline) ""
+    where numP  = try regP <|> pairP
+          pairP = Pair <$> (char '[' *> numP <* char ',') <*> (numP <* char ']')
+          regP  = Regular <$> read <$> many1 digit
+          must (Right x) = x
+          must (Left y)  = error $ show y
 
 -- Returns Just if value is changed, Nothing otherwise
 explode' :: Int -> Number -> Maybe (Number, (Int, Int))
 explode' _ (Regular x) = Nothing
 explode' 4 (Pair (Regular l) (Regular r)) = Just (Regular 0, (l,r))
-explode' depth (Pair l r) = case explode' (depth+1) l of
-    Just (expL, (lVal,rVal)) -> Just (Pair expL (addL rVal r), (lVal,0))
-    Nothing                  -> case explode' (depth+1) r of
-        Just (expR, (lVal,rVal)) -> Just (Pair (addR lVal l) expR, (0,rVal))
-        Nothing                  -> Nothing
-
--- Add to the given number's leftmost Regular value
-addL :: Int -> Number -> Number
-addL n (Regular x) = Regular (x+n)
-addL n (Pair l r)  = Pair (addL n l) r
-
--- Add to the given number's rightmost Regular value
-addR :: Int -> Number -> Number
-addR n (Regular x) = Regular (x+n)
-addR n (Pair l r)  = Pair l (addR n r)
-
-split :: Number -> Number
-split num = fromMaybe num (split' num)
+explode' d (Pair l r) =
+        (explode' (d+1) l <&> (\(n, (l',r')) -> (Pair n (addL r' r), (l',0))))
+    <|> (explode' (d+1) r <&> (\(n, (l',r')) -> (Pair (addR l' l) n, (0,r'))))
+    where addL n (Regular x) = Regular (x+n)
+          addL n (Pair l r)  = Pair (addL n l) r
+          addR n (Regular x) = Regular (x+n)
+          addR n (Pair l r)  = Pair l (addR n r)
 
 -- Returns Just if value is changed, Nothing otherwise
 split' :: Number -> Maybe Number
-split' (Regular x) = if x >= 10
-    then Just $ Pair (Regular $ div x 2) (Regular $ (div x 2) + (mod x 2))
-    else Nothing
-split' (Pair l r)  = case split' l of
-    (Just num) -> Just $ Pair num r
-    Nothing    -> case split' r of
-        (Just num) -> Just $ Pair l num
-        Nothing    -> Nothing
+split' (Regular x) = if x < 10 then Nothing else
+    Just $ Pair (Regular (div x 2)) (Regular ((div x 2) + (mod x 2)))
+split' (Pair l r) = (split' l <&> (\n -> Pair n r))
+                <|> (split' r <&> (\n -> Pair l n))
 
 add :: Number -> Number -> Number
 add l r = reduce (Pair l r)
-
-pairs vals = [(x,y) | x <- vals, y <- vals, x /= y]
+    where reduce = fix (split . fix explode)
+          fix f x = if x == f x then x else fix f (f x)
+          explode num = maybe num fst (explode' 0 num)
+          split num = maybe num id (split' num)
 
 magnitude :: Number -> Int
 magnitude (Regular x) = x
 magnitude (Pair l r)  = (3 * magnitude l) + (2 * magnitude r)
 
 partOne = magnitude . foldl1 add
-partTwo = maximum . map (magnitude . uncurry add) . pairs
+partTwo = maximum . map magnitude . pairs
+    where pairs nums = [add x y | x <- nums, y <- nums, x /= y]
 
-main = interact $ (++ "\n") . show . sequence [partOne, partTwo] . must . parse inputP ""
+main = interact $ (++ "\n") . show . sequence [partOne, partTwo] . parseInput
