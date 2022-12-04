9db7bbaf97582f1c3c3db5523cde4ff9054dcd05
David Crocker <david@crocker.im>
2022-12-04 00:45:28 -0500

    2022 day 04 golfing

--- 2022/04.hs
+++ 2022/04.hs
@@ -1,24 +1,11 @@
-import Advent
-import Data.List
+parse :: String -> [((Int, Int), (Int, Int))]
+parse = map (both (both read . splitOn '-') . splitOn ',') . lines
+    where splitOn c xs = fmap tail $ break (==c) xs
+          both f (x,y) = (f x, f y)
 
-parse :: String -> [[(Int, Int)]]
-parse = map (map (toPair . map read . splitOn '-') . splitOn ',') . lines
-    where toPair [x,y] = (x,y)
-
-rangeToList :: (Int, Int) -> [Int]
-rangeToList (x,y) = [x..y]
-
-contains :: Eq a => [a] -> [a] -> Bool
-contains xs ys = union ys xs == ys
-
-fullOverlap :: Eq a => [[a]] -> Bool
-fullOverlap xs = any (contains everything) xs
-    where everything = foldr1 union xs
-
-anyOverlap :: Eq a => [[a]] -> Bool
-anyOverlap xs = not (null (foldr1 intersect xs))
-
-partOne = length . filter fullOverlap . map (map rangeToList)
-partTwo = length . filter anyOverlap . map (map rangeToList)
+partOne = length . filter
+    (\((x1,y1),(x2,y2)) -> (x1 >= x2 && y1 <= y2) || (x2 >= x1 && y2 <= y1))
+partTwo = length . filter
+    (\((x1,y1),(x2,y2)) -> (x1 <= y2 && y1 >= x2) || (x2 <= y1 && y2 >= y1))
 
 main = interact $ (++ "\n") . show . sequence [partOne, partTwo] . parse
