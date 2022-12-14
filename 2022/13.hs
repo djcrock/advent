5f1408b2c93878299bc45617203574483096c32e
David Crocker <david@crocker.im>
2022-12-13 19:35:31 -0500

    2022 day 13 cleanup

--- 2022/13.hs
+++ 2022/13.hs
@@ -1,32 +1,28 @@
 import Data.List ( findIndices, sort )
 import Text.Parsec
 
-data Packet = List [Packet] | Val Int deriving ( Eq )
+data Packet = L [Packet] | V Int deriving Eq
 
 parseInput :: String -> [[Packet]]
 parseInput = must . parse (sepBy pairP newline) ""
     where must    = either (error . show) id
           pairP   = sepEndBy packetP newline
           packetP = try listP <|> valP
-          listP   = List <$> (char '[' *> sepBy packetP (char ',') <* char ']')
-          valP    = Val . read <$> many1 digit
+          listP   = L <$> (char '[' *> sepBy packetP (char ',') <* char ']')
+          valP    = V . read <$> many1 digit
           
 instance Ord Packet where
-    compare (List [])     (List (_:_))  = LT
-    compare (List (_:_))  (List [])     = GT
-    compare (List [])     (List [])     = EQ
-    compare (Val a)       (Val b)       = compare a b
-    compare a@(Val _)     b@(List _)    = compare (List [a]) b
-    compare a@(List _)    b@(Val _)     = compare a (List [b])
-    compare (List (a:as)) (List (b:bs)) = case compare a b of
-                                              EQ -> compare (List as) (List bs)
-                                              x  -> x
+    compare (L [])     (L [])     = EQ
+    compare (L [])     _          = LT
+    compare _          (L [])     = GT
+    compare (V a)      (V b)      = compare a b
+    compare a@(V _)    b@(L _)    = compare (L [a]) b
+    compare a@(L _)    b@(V _)    = compare a (L [b])
+    compare (L (a:as)) (L (b:bs)) = compare a b <> compare (L as) (L bs)
 
-partOne = sum . map fst . filter (isSorted . snd) . zip [1..]
-    where isSorted xs = xs == sort xs
+partOne = sum . map (+1) . findIndices (\xs -> xs == sort xs)
 
-partTwo = product . indices dividers . sort . (dividers ++) . concat
-    where dividers   = [List [List [Val 2]], List [List [Val 6]]]
-          indices xs = map (+1) . findIndices (`elem` xs)
+partTwo = product . map (+1) . findIndices (`elem` divs) . sort . concat . (divs:)
+    where divs = [L [L [V 2]], L [L [V 6]]]
 
 main = interact $ (++ "\n") . show . sequence [partOne, partTwo] . parseInput
